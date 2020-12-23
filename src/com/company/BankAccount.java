31a75db3c3848807284b5f48578a8f80628fdb01
Aikeldi <aykeldi@inbox.ru>
2020-12-23 21:06:06 +0600

    Initial commit

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class BankAccount {
+    private double amount;
+
+
+    public BankAccount(){
+
+    }
+    public BankAccount (double amount){
+        this.amount = amount;
+
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public double deposit(double sum) {
+        return this.amount += sum;
+
+    }
+    public double withDraw(double sum) throws LimitException {
+        if(sum > this.amount) {
+            throw new LimitException("Ошибка, снимаемая сумма больше чем остаток на балансе", this.amount);
+
+        } else {
+            return this.amount -= sum;
+        }
+    }
+}
